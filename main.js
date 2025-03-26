f9b582db1d8094387099612a029299d386af9141
lujehle <lukas-jehle@web.de>
2025-03-26 17:49:14 +0100

    Koordinaten von Sardinien statt Neuseeland eingefügt

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var map = L.map('map').setView([-41.270833, 173.284167], 11);
+var map = L.map('map').setView([40.027778, 9.069444], 7);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
-        var marker = L.marker([-41.270833, 173.284167]).addTo(map);
+        var marker = L.marker([40.027778, 9.069444]).addTo(map);
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
